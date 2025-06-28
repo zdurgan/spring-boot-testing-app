f54cca90f5a98d44f0b4e0757e53d9b67a7b90cd
Zachary Durgan <zdurgan@gmail.com>
2025-06-28 17:23:05 -0400

    Spring Initializer

---
+++ src/main/java/com/example/spring_boot_testing_app/SpringBootTestingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_testing_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_testing_app/SpringBootTestingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_testing_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
